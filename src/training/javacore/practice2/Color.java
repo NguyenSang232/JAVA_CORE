c1a5223dac5db772f2dbb203583417ad6e316fe1
ThanhSang <ThanhSangNguyen>
2026-06-08 14:30:29 +0700

    Add enum

--- src/training/javacore/practice2/Color.java
+++ src/training/javacore/practice2/Color.java
@@ -6,3 +6,9 @@ package training.javacore.practice2;
 public enum Color {
 	RED, BLUE, YELLOW, GREEN;
 }
+//Câu 4f Nên tạo Enum Color vì nó cố định, nên được cài đặt sẵn trong trường
+// hợp người nhập sai
+// không đúng định dạng ví dụ RED mà dùng nhập rED.
+//Câu 4f Nên tạo Enum Color vì nó cố định, nên được cài đặt sẵn trong trường
+// hợp người nhập sai
+// không đúng định dạng ví dụ RED mà dùng nhập rED.
